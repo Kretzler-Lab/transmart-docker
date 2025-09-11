536478548c136718c3505995bc39e3cb22f985d9
dert1129 <nathancreger1111@gmail.com>
2025-09-11 13:25:42 -0400

    point at home dir

--- transmart-app/Rscripts/Survival/BuildSurvivalData.R
+++ transmart-app/Rscripts/Survival/BuildSurvivalData.R
@@ -155,7 +155,7 @@ snptype.category = ''
 	#Write the final data file.
 	#write.matrix(finalData,"outputfile",sep = "\t")
 	# Using write.table; write.matrix was leaving trailing white-space in the file - see JIRA issue TRANSREL-24.
-    write.csv(finalData,"/tmp/finalDataDebug.csv",row.names=TRUE)
+    write.csv(finalData,"~/finalDataDebug.csv", row.names=TRUE)
     write.table(finalData,filename, sep = "\t", quote = FALSE, row.names = FALSE)
     print("-------------------")
 }
