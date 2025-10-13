b7611585578995458015dbb4e2734211999e13a1
Zach Wright <zwright@umich.edu>
2025-10-13 14:32:57 -0400

    TM-868: loading scripts

---
+++ load_clinical_docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if (( $# == 0 )); then
+  echo "Please enter a study name."
+else
+  docker run -ti --rm --network transmart -v /app/transmart/transmart-docker/studies/$1:/home/tmload/transmart-data/samples/studies/$1 -e JAVAMAXMEM='4096' kretzlerdevs/transmart-load:1.1 /bin/bash load_clinical.sh $1
+fi
